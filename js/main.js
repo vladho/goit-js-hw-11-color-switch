e51e383b86e4fc37a55d9b6bab84b99789a3f649
Vlad <vladhopilo@gmail.com>
2021-01-21 16:56:57 +0200

    done

--- js/main.js
+++ js/main.js
@@ -11,4 +11,33 @@ const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-console.log("ghefw");
+const startRef = document.querySelector('button[data-action="start"]')
+const stopRef = document.querySelector('button[data-action="stop"]')
+
+const bodyRef = document.querySelector("body")
+
+let timerId = null;
+let isActive = false;
+
+startRef.addEventListener("click", () => {
+  if (isActive) {
+    return
+  }
+
+  isActive = true;
+  bodyRef.classList.add("color")
+  const colorRef = document.querySelector(".color")
+  const randomColor = () => {
+    colorRef.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+  }
+  timerId = setInterval(randomColor, 3000)
+})
+
+stopRef.addEventListener("click", () => {
+  clearInterval(timerId)
+  timerId = null;
+  isActive = false;
+})
+
+
+ 
