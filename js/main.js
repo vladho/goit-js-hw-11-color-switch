98b924f380ef439db4cbdcd7ffadbdde1ca3b5c5
vladho <vladhopilo@gmail.com>
2021-01-27 18:42:44 +0200

    upd

--- js/main.js
+++ js/main.js
@@ -1,41 +1,31 @@
-const colors = [
-  "#FFFFFF",
-  "#2196F3",
-  "#4CAF50",
-  "#FF9800",
-  "#009688",
-  "#795548",
-];
+const colors = ["#FFFFFF", "#2196F3", "#4CAF50", "#FF9800", "#009688", "#795548"]
 
 const randomIntegerFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
 
-const startRef = document.querySelector('button[data-action="start"]');
-const stopRef = document.querySelector('button[data-action="stop"]');
-const bodyRef = document.querySelector("body");
+const startRef = document.querySelector('button[data-action="start"]')
+const stopRef = document.querySelector('button[data-action="stop"]')
+const bodyRef = document.querySelector("body")
 
-let timerId = null;
-let isActive = false;
+let timerId = null
+let isActive = false
 
 startRef.addEventListener("click", () => {
   if (isActive) {
-    return;
+    return
   }
 
-  isActive = true;
-  bodyRef.classList.add("color");
-  const colorRef = document.querySelector(".color");
+  isActive = true
+
   const randomColor = () => {
-    colorRef.style.backgroundColor =
-      colors[randomIntegerFromInterval(0, colors.length - 1)];
-  };
-  randomColor();
-  timerId = setInterval(randomColor, 1000);
-});
+    bodyRef.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)]
+  }
+  randomColor()
+  timerId = setInterval(randomColor, 1000)
+})
 
 stopRef.addEventListener("click", () => {
-  timerId = null;
-  isActive = false;
-  clearInterval(timerId);
-});
+  isActive = false
+  clearInterval(timerId)
+})
